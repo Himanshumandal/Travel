d523484ce0d0c25027faf000ba090dd55a9c880e
Himanshu Mandal <113971156+Himanshumandal@users.noreply.github.com>
2024-09-19 12:25:51 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Hero from "@/components/Hero";
+import Camp from "@/components/Camp"
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+
+export default function Home() {
+  return (
+   <>
+
+   <Hero/>
+   <Camp/>
+   <Guide/>
+   <Features/>
+   <GetApp/>
+        
+   </>
+  );
+}
